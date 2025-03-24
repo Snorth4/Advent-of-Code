636a530a3537f20863928a4144567b9c8ed99e9f
Snorth4 <snorth4@huskers.unl.edu>
2025-03-24 11:04:28 -0500

    Day 4.1 Finished

---
+++ 2015/Day 4.1.py
@@ -0,0 +1,27 @@
+import hashlib
+
+char = "0"
+num_char = 5
+
+def main():
+
+    print("Input:")
+    string = str(input())
+
+    number = 0
+    valid = False
+    while not valid:
+        
+        number += 1
+        string_i = f"{string}{number}"
+        string_encoding = string_i.encode()
+        hash = hashlib.md5(string_encoding)
+        hash_str = hash.hexdigest()
+
+        if hash_str[0:5] == char * num_char:
+            valid = True
+
+    print(f"Number = {number}")
+
+if __name__ == "__main__":
+    main()
