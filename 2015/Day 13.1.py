fc43e696f84ced6665fab6bf9afce718dd110d88
Snorth4 <snorth4@huskers.unl.edu>
2025-05-15 13:00:04 -0500

    Day 13.1 Finished

---
+++ 2015/Day 13.1.py
@@ -0,0 +1,47 @@
+import math
+from itertools import permutations
+from collections import defaultdict
+
+def main():
+    print("Input:")
+    
+    words: list[list[str]] = [[]]
+    while True:
+        try:
+            word = input()
+            if word.strip() == "":
+                break
+            
+            words.append(word.split(" "))
+        except EOFError:
+            break
+    
+    words = [x for x in words if x] 
+    guest_list: list[str] = []
+    happiness_values = defaultdict(dict[str, int])
+    
+    for i in range(len(words)):
+        guest_list.append(words[i][0])
+        happiness_values[words[i][0]][words[i][10][:-1]] = int(words[i][3]) * (-1 if words[i][2] == "lose" else 1)
+    del words
+    
+    guest_list = list(set(guest_list))
+    guest_list.sort()
+    n = len(guest_list)
+    
+    max_happiness = -float("inf")
+    for perm in permutations(guest_list[1:]):
+        perm = list(perm)
+        perm.insert(0, guest_list[0])
+        
+        happiness = 0
+        for i in range(n):
+            happiness += happiness_values[perm[i-1]][perm[i]]
+            happiness += happiness_values[perm[i]][perm[i-1]]
+        if happiness > max_happiness:
+            max_happiness = happiness
+    
+    print(f"Maximum happiness: {max_happiness}")
+        
+if __name__ == "__main__":
+    main()
