f5fb23ad610b514344afed8fc99b27c6655e3d9f
Snorth4 <snorth4@huskers.unl.edu>
2025-08-18 21:02:54 -0500

    Day 20.1 Finished

---
+++ 2015/Day 20.1.py
@@ -0,0 +1,40 @@
+import math, time
+
+def main():
+
+    ### Process the input...
+
+    print("Integer Input:")
+
+    threshold: int = int(input())
+    reduced_threshold: int = math.ceil(float(threshold) / 10.0)
+    i: int = 0
+    house_value: int = 0
+    
+    start_time = time.time()
+    
+    while house_value < reduced_threshold:
+        i += 1
+        house_value = 0
+        for j in range(1, i+1):
+            if i % j == 0:
+                house_value += j
+    
+    print(f"Lowest number house with at least as many presents as the house number: {i}")
+    print(f"Runtime: {(((time.time() - start_time)*1000) // 10) / 100:.2f}s")
+    print()
+
+if __name__ == "__main__":
+    main()
+
+# 10000  - 360
+# Runtime: 0:00:00.00s
+# 
+# 100000 - 3120
+# Runtime: 0:00:00.11s
+# 
+# 1000000 - 27720
+# Runtime: 0:00:09.80s
+# 
+# 29000000 - 665280
+# Runtime: 1:54:00.49s
